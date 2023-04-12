533424337a0fa1b8b17f23a99af2c910d4def9d7
hassansolangi <hassansolangi2021@gmail.com>
2023-04-12 21:03:25 +0500

    10th commit completed profile and sign-in pagestate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 export default function Header() {
+  const [pageState, setPageState] = useState("Sign In")
   const location = useLocation();
   const navigate = useNavigate();
+  const auth = getAuth();
+  useEffect(()=>{
+    onAuthStateChanged(auth, (user)=>{
+      if(user){
+        setPageState("Profile")
+      } else{
+        setPageState("Sign In")
+      }
+    })
+  }, [auth])
 
   function pathMatchRoute(route) {
     if (route === location.pathname) {
@@ -42,11 +54,11 @@ export default function Header() {
             </li>
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMatchRoute("/sign-in") && "text-black border-b-red-500"
+                (pathMatchRoute("/sign-in") || pathMatchRoute("/profile"))  && "text-black border-b-red-500"
               }`}
-              onClick={() => navigate("/sign-in")}
+              onClick={() => navigate("/profile")}
             >
-              Sign In
+              {pageState}
             </li>
           </ul>
         </div>
